5e8763fee0150b205a5ed05cec1ec6d163544d9c
vincent-eboya613 <vincentkidavi@gmail.com>
2025-07-04 22:34:08 +0300

    Creating Card.tsx

---
+++ components/common/Card.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface ButtonProps {
+    label: string;
+    onClick: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
+    return (
+        <button
+            onClick={onClick}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+            {label}
+        </button>
+    );
+};
+
+export default Button;
